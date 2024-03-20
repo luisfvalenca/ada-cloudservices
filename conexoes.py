fdbc5206e60d7e9d76cae8158ee99c3e844ac9a3
luisfvalenca <luisfvalenca@gmail.com>
2024-03-20 10:33:56 -0300

    Ajustes em conexoes.py

--- conexoes.py
+++ conexoes.py
@@ -3,10 +3,7 @@ import redis
 from minio import Minio
 
 def connect_rabbitmq():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host="localhost",
-        port=5672,
-        virtual_host="/"))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost", port=5672, virtual_host="/"))
     channel = connection.channel()
     return channel
 
@@ -14,8 +11,4 @@ def connect_redis():
     return redis.Redis(host='localhost', port=6379, db=0)
 
 def connect_minio():
-    return Minio(
-        endpoint="localhost:9000",
-        access_key="minioadmin",
-        secret_key="minioadmin",
-        secure=False)
+    return Minio(endpoint="localhost:9000", access_key="minioadmin", secret_key="minioadmin", secure=False)
